7142e7aa9e52050df722a5167d1df55060c34758
mmikkka <mikovarina333@gmail.com>
2026-01-08 21:47:48 +0500

    refactor: вынос логики взаимодействия с API фильмов в отдельный сервис

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ import type { PaginationParams } from "./types/common";
 
 export const API_BASE_URL = "http://91.142.94.183:8080";
 
-export const PLACEHOLDER_POSTER = "https://placehold.co/300x450";
+export const PLACEHOLDER_POSTER = "https://placehold.com";
 
 export const MESSAGES = {
   authRequired: "Сначала авторизуйтесь",
